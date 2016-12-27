92d54a28ffc9df735ad320e3524348ffb3fced41
sandeepyadav1 <sandy23288@gmail.com>
2016-12-27 11:52:42 +0530

    Updates customer form design!

--- WindowsFormsApplication1/Customer.Designer.cs
+++ WindowsFormsApplication1/Customer.Designer.cs
@@ -602,9 +602,10 @@
             this.panel1.Controls.Add(this.butclose);
             this.panel1.Controls.Add(this.butupdate);
             this.panel1.Controls.Add(this.butaddrecord);
-            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(802, 593);
+            this.panel1.Size = new System.Drawing.Size(803, 594);
             this.panel1.TabIndex = 11;
             // 
             // groupBox3
