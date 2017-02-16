3179d0830f05290a472001e552360866eb118050
sandeepyadav1 <sandy23288@gmail.com>
2017-02-16 10:40:03 +0530

    Some changes in company form

--- WindowsFormsApplication1/Company_New.Designer.cs
+++ WindowsFormsApplication1/Company_New.Designer.cs
@@ -93,6 +93,9 @@
             this.ComDetails = new System.Windows.Forms.ComboBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -602,7 +605,7 @@
             this.panel1.Controls.Add(this.ComDetails);
             this.panel1.Location = new System.Drawing.Point(0, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(803, 647);
+            this.panel1.Size = new System.Drawing.Size(803, 630);
             this.panel1.TabIndex = 32;
             // 
             // btnPrint
@@ -659,7 +662,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(18, 77);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(761, 506);
+            this.dataGridView1.Size = new System.Drawing.Size(761, 303);
             this.dataGridView1.StandardTab = true;
             this.dataGridView1.TabIndex = 5;
             this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick_1);
@@ -709,6 +712,9 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.comboBox1);
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.checkvat);
             this.groupBox2.Controls.Add(this.textBox3);
@@ -735,6 +741,30 @@
             this.textBox1.TabStop = false;
             this.textBox1.Text = "%";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(450, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 35;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(587, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 36;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(693, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 23);
+            this.button1.TabIndex = 37;
+            this.button1.Text = "..";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Company_New
             // 
@@ -831,6 +861,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnPrint;
         private System.Windows.Forms.Button btnExportToExcel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
 
 
     }
