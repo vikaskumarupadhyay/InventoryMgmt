f8e8046582d5a18cda624ad5d03b2ce452bce6c6
Dinesh Tiwari <Dinesh Tiwari>
2017-03-03 14:56:12 +0530

    comit item page

--- WindowsFormsApplication1/Item.Designer.cs
+++ WindowsFormsApplication1/Item.Designer.cs
@@ -515,7 +515,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1172, 549);
             this.panel1.TabIndex = 0;
-           // this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // panel2
             // 
