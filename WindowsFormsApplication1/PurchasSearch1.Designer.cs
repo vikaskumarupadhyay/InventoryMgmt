c17cb4552c3a1e12e15b67a9fe5d4d3d4ac59e4b
Umesh <Umesh>
2017-04-06 20:04:38 +0530

    umesh Change 6-Apr-17

--- WindowsFormsApplication1/PurchasSearch1.Designer.cs
+++ WindowsFormsApplication1/PurchasSearch1.Designer.cs
@@ -33,6 +33,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtsearch = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.gridPurchaseSearch)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,9 +43,9 @@
             // 
             this.gridPurchaseSearch.AllowUserToAddRows = false;
             this.gridPurchaseSearch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridPurchaseSearch.Location = new System.Drawing.Point(12, 100);
+            this.gridPurchaseSearch.Location = new System.Drawing.Point(12, 69);
             this.gridPurchaseSearch.Name = "gridPurchaseSearch";
-            this.gridPurchaseSearch.Size = new System.Drawing.Size(667, 350);
+            this.gridPurchaseSearch.Size = new System.Drawing.Size(1330, 381);
             this.gridPurchaseSearch.TabIndex = 0;
             // 
             // comboPurchasesearch
@@ -82,11 +85,40 @@
             this.txtsearch.TabIndex = 4;
             this.txtsearch.TextChanged += new System.EventHandler(this.txtsearch_TextChanged);
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(562, 38);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(135, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(933, 38);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(117, 20);
+            this.dateTimePicker2.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(1196, 25);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(120, 33);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
             // PurchasSearch1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 462);
+            this.ClientSize = new System.Drawing.Size(1364, 462);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.txtsearch);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -109,5 +141,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button btnSave;
     }
 }
