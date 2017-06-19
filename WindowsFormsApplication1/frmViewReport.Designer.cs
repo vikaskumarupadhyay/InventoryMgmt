9215b8d84be1658c6debcb9110ee8a362c548484
sandeepyadav1 <sandy23288@gmail.com>
2017-06-19 16:11:22 +0530

    bdbdb

---
+++ WindowsFormsApplication1/frmViewReport.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmViewReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CrViewer = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // CrViewer
+            // 
+            this.CrViewer.ActiveViewIndex = -1;
+            this.CrViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CrViewer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.CrViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CrViewer.Location = new System.Drawing.Point(0, 0);
+            this.CrViewer.Name = "CrViewer";
+            this.CrViewer.Size = new System.Drawing.Size(770, 424);
+            this.CrViewer.TabIndex = 0;
+            // 
+            // frmViewReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 424);
+            this.Controls.Add(this.CrViewer);
+            this.Name = "frmViewReport";
+            this.Text = "View Report";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public CrystalDecisions.Windows.Forms.CrystalReportViewer CrViewer;
+
+    }
+}
