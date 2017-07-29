221e7eb0fec2ff7c6de18d36689e3cc747f87d6e
Dinesh Tiwari <Dinesh Tiwari>
2017-07-29 17:16:26 +0530

    all pages changes

--- WindowsFormsApplication1/SalesOrder.Designer.cs
+++ WindowsFormsApplication1/SalesOrder.Designer.cs
@@ -712,6 +712,7 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1172, 634);
             this.panel2.TabIndex = 60;
+           // this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // crystalReportViewer1
             // 
